6909367c574fa632a9c005223d920092e928d754
Jason Mun <jasonmun@yahoo.com>
2017-12-23 13:40:58 +0800

    2017-12-23 13:40

--- auto-git@yahoo.com/sh/clear-history.sh
+++ auto-git@yahoo.com/sh/clear-history.sh
@@ -6,6 +6,8 @@ DT=`date '+%Y-%m-%d %H:%M'`
 
 branch_name=`git -C "${GIT_PATH}" branch --no-color | cut -c3-`
 
+SSH_URL=`git -C "${GIT_PATH}" remote get-url origin`
+
 git -C "${GIT_PATH}" checkout --orphan latest_branch
 
 git -C "${GIT_PATH}" add -A
@@ -16,4 +18,6 @@ git -C "${GIT_PATH}" branch -D ${branch_name}
 
 git -C "${GIT_PATH}" branch -m ${branch_name}
 
-git -C "${GIT_PATH}" push -f origin ${branch_name}
+# git -C "${GIT_PATH}" push -f origin ${branch_name}
+
+git -C "${GIT_PATH}" push -f ${SSH_URL}
